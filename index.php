c63be7309370c1e07a1ca8e588f4c03387673aa0
Robert Booth <rbooth@zenoss.com>
2013-08-26 10:12:54 -0500

    Update to textarea

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
     <form method="post">
         UserName: <input type="text" name="username"><br>
-        SSH Public Key: <textarea cols="40" rows="10" name="sshkey"/><br>
+        SSH Public Key: <textarea cols="40" rows="10" name="sshkey"></textarea><br>
         Department: <select>
                         <option value="solutions">Solutions</option>
                         <option value="qa">QA</option>
